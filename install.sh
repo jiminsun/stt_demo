b4dbe4d4e4a8ea17b9ca7cbfe4093a09fc98c11a
Jimin Sun <jiminxsun@gmail.com>
2022-04-02 01:55:16 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,16 +2,20 @@ sudo apt-get update
 sudo apt-get install -y libportaudio2 ffmpeg
 
 
-ROOT=$PWD
+STT_ROOT=$PWD
 pip install -r requirements.txt
 
+if [ -f "$STT_ROOT/fairseq" ]; then
+  rm -rf $STT_ROOT/fairseq
+fi
+  
 git clone https://github.com/pytorch/fairseq.git
 cd fairseq
 pip install --editable ./
 
 
 # Overwrite speech_generator.py with the edited version
-cd $ROOT
+cd $STT_ROOT
 pip install sounddevice
 pip install g2p-en
 
